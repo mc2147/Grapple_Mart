98af298c6bca0774e841726f88c43b718122ba60
Dmitry Vasilev <scythargon@gmail.com>
2017-10-24 12:55:36 +0300

    Replace tabs with spaces

--- Grapple_Mart/Instructors/models.py
+++ Grapple_Mart/Instructors/models.py
@@ -11,32 +11,32 @@ from django.db import models
 
 # # Create your models here.
 # class Instructor(models.Model):
-# 	User = models.OneToOneField(User)
-# 	# Followers = models.ManyToManyField('Athlete', default="")
-# 	Name = models.CharField(default="", max_length=100)
-# 	Bio = HTMLField()
+#     User = models.OneToOneField(User)
+#     # Followers = models.ManyToManyField('Athlete', default="")
+#     Name = models.CharField(default="", max_length=100)
+#     Bio = HTMLField()
 
 # class E_Book(models.Model):
-# 	Owner = models.OneToOneField(Instructor, default="")
-# 	# Buyers = models.ManyToManyField(Athlete, default="")
+#     Owner = models.OneToOneField(Instructor, default="")
+#     # Buyers = models.ManyToManyField(Athlete, default="")
 
 # class Video_Product(models.Model):
-# 	Owner = models.OneToOneField(Instructor, default="")
-# 	# Buyers = models.ManyToManyField(Athlete, default="")
+#     Owner = models.OneToOneField(Instructor, default="")
+#     # Buyers = models.ManyToManyField(Athlete, default="")
 
 # class Course(models.Model):
-# 	Owner = models.OneToOneField(Instructor, default="")
-# 	Title = models.CharField(default="", max_length=100)
-# 	Description = models.CharField(default="", max_length=1000)
-# 	Topic = models.CharField(default="", max_length=20)
-# 	Owner = models.OneToOneField(Instructor, default="")
-# 	# Buyers = models.ManyToManyField(Athlete, default="")
-# 	Confirmed = models.BooleanField(default=False)
+#     Owner = models.OneToOneField(Instructor, default="")
+#     Title = models.CharField(default="", max_length=100)
+#     Description = models.CharField(default="", max_length=1000)
+#     Topic = models.CharField(default="", max_length=20)
+#     Owner = models.OneToOneField(Instructor, default="")
+#     # Buyers = models.ManyToManyField(Athlete, default="")
+#     Confirmed = models.BooleanField(default=False)
 
 # class Course_Item(models.Model):
-# 	# Course = models.ForeignKey(Course, related_name="Items")
-# 	Type = models.CharField(default="", max_length=20)
-# 	Ordered_ID = models.IntegerField(default=0)
+#     # Course = models.ForeignKey(Course, related_name="Items")
+#     Type = models.CharField(default="", max_length=20)
+#     Ordered_ID = models.IntegerField(default=0)
 
 # class Course_Files(models.Model):
-# 	Course = models.OneToOneField(Course, default="")
+#     Course = models.OneToOneField(Course, default="")
--- Grapple_Mart/User/forms.py
+++ Grapple_Mart/User/forms.py
@@ -2,4 +2,4 @@ from django import forms
 from tinymce.models import HTMLField
 
 class Instructor_Bio_Form(forms.Form):
-	Bio = HTMLField()
+    Bio = HTMLField()
